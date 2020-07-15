b83b4cb66a6fab0663051c18030518affdfba283
hiei2716 <s-k.exvs.only@ezweb.ne.jp>
2020-07-15 12:16:51 +0900

    マイグレーション(1)

--- db/migrate/20200715030822_create_program_words.rb
+++ db/migrate/20200715030822_create_program_words.rb
@@ -2,7 +2,7 @@ class CreateProgramWords < ActiveRecord::Migration[5.2]
   def change
     create_table :program_words do |t|
       t.name
-      t.text
+      t.content
       t.timestamps
     end
   end
--- db/migrate/20200715030904_create_program_frameworks.rb
+++ db/migrate/20200715030904_create_program_frameworks.rb
@@ -1,7 +1,8 @@
 class CreateProgramFrameworks < ActiveRecord::Migration[5.2]
   def change
     create_table :program_frameworks do |t|
-
+      t.name
+      t.content
       t.timestamps
     end
   end
